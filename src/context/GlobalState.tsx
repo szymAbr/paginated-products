11e4b49a36024a26e55c46fe0d1eeced515107bd
szymAbr <sz.abramowski@gmail.com>
2022-06-07 16:50:49 +0200

    create context

---
+++ src/context/GlobalState.tsx
@@ -0,0 +1,58 @@
+import { createContext, useReducer } from "react";
+
+const initialState = {
+  products: {
+    page: 0,
+    per_page: 0,
+    total: 0,
+    total_pages: 0,
+    data: [],
+  },
+};
+
+type AppState = typeof initialState;
+
+type Action = {
+  type: "SET_PRODUCTS";
+  payload: AppState;
+};
+
+export function createGlobalState<StateType, ActionType>(
+  reducer: React.Reducer<StateType, ActionType>,
+  initialState: StateType
+) {
+  const defaultDispatch: React.Dispatch<ActionType> = () => initialState;
+
+  const globalState = createContext({
+    state: initialState,
+    dispatch: defaultDispatch,
+  });
+
+  function Provider(props: React.PropsWithChildren<{}>) {
+    const [state, dispatch] = useReducer<React.Reducer<StateType, ActionType>>(
+      reducer,
+      initialState
+    );
+
+    return <globalState.Provider value={{ state, dispatch }} {...props} />;
+  }
+
+  return [globalState, Provider] as const;
+}
+
+function reducer(state: AppState, action: Action): AppState {
+  switch (action.type) {
+    case "SET_PRODUCTS":
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export const [GlobalState, GlobalProvider] = createGlobalState(
+  reducer,
+  initialState
+);
