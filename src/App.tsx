32fd7933138617dd0b12ffc46868733168730bc4
szymAbr <sz.abramowski@gmail.com>
2022-06-08 21:43:12 +0200

    add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
-import Products from "./components/Products";
 import { GlobalProvider } from "./context/GlobalState";
+import ProductTable from "./components/ProductTable";
+import { Container } from "@mui/system";
+import { Backdrop, CircularProgress } from "@mui/material";
 
 export default function App() {
-  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <GlobalProvider>
-      <div className="app">
-        <Products />
-      </div>
+      <Container>
+        <ProductTable loading={loading} setLoading={setLoading} />
+
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </Container>
     </GlobalProvider>
   );
 }
